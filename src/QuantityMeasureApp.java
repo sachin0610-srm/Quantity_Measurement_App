ba06f9e82fe55599ccc4243fc45beed2694eec25
sachin0610-srm <sachinlava14@gmail.com>
2026-04-17 15:25:49 +0530

    UC1: Feet measurement equality

--- src/QuantityMeasureApp.java
+++ src/QuantityMeasureApp.java
@@ -1,2 +1,39 @@
 public class QuantityMeasureApp {
+
+    public static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+
+            Feet feet = (Feet) obj;
+            return Double.compare(feet.value, this.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+
+    public static void main(String[] args) {
+        Feet firstFeet = new Feet(1.0);
+        Feet secondFeet = new Feet(1.0);
+        Feet thirdFeet = new Feet(2.0);
+
+        System.out.println("Comparing 1.0 ft and 1.0 ft: " + firstFeet.equals(secondFeet)); // true
+        System.out.println("Comparing 1.0 ft and 2.0 ft: " + firstFeet.equals(thirdFeet));  // false
+        System.out.println("Comparing 1.0 ft and null: " + firstFeet.equals(null));        // false
+    }
 }
