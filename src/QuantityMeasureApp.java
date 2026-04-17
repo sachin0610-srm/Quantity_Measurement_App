63a057720fe296e11f0069dbc43fe439852d16a7
sachin0610-srm <sachinlava14@gmail.com>
2026-04-17 15:29:51 +0530

    UC2: Feet and Inches measurement equality

--- src/QuantityMeasureApp.java
+++ src/QuantityMeasureApp.java
@@ -1,39 +1,58 @@
 public class QuantityMeasureApp {
 
-    public static class Feet {
-        private final double value;
+    public static boolean compareFeet(double val1, double val2) {
+        Feet f1 = new Feet(val1);
+        Feet f2 = new Feet(val2);
+        return f1.equals(f2);
+    }
 
-        public Feet(double value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }
-
-            if (obj == null || getClass() != obj.getClass()) {
-                return false;
-            }
-
-            Feet feet = (Feet) obj;
-            return Double.compare(feet.value, this.value) == 0;
-        }
-
-        @Override
-        public int hashCode() {
-            return Double.hashCode(value);
-        }
+    public static boolean compareInches(double val1, double val2) {
+        Inches i1 = new Inches(val1);
+        Inches i2 = new Inches(val2);
+        return i1.equals(i2);
     }
 
     public static void main(String[] args) {
-        Feet firstFeet = new Feet(1.0);
-        Feet secondFeet = new Feet(1.0);
-        Feet thirdFeet = new Feet(2.0);
+        System.out.println("Input: 1.0 inch and 1.0 inch | Output: " + compareInches(1.0, 1.0));
+        System.out.println("Input: 1.0 ft and 1.0 ft     | Output: " + compareFeet(1.0, 1.0));
+        System.out.println("Input: 1.0 ft and 2.0 ft     | Output: " + compareFeet(1.0, 2.0));
+    }
+}
 
-        System.out.println("Comparing 1.0 ft and 1.0 ft: " + firstFeet.equals(secondFeet)); // true
-        System.out.println("Comparing 1.0 ft and 2.0 ft: " + firstFeet.equals(thirdFeet));  // false
-        System.out.println("Comparing 1.0 ft and null: " + firstFeet.equals(null));        // false
+class Feet {
+    private final Double value;
+
+    public Feet(Double value) {
+        if (value == null) {
+            throw new IllegalArgumentException("Value cannot be null");
+        }
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Feet feet = (Feet) o;
+        return Double.compare(feet.value, value) == 0;
+    }
+}
+
+class Inches {
+    private final Double value;
+
+    public Inches(Double value) {
+        if (value == null) {
+            throw new IllegalArgumentException("Value cannot be null");
+        }
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Inches inches = (Inches) o;
+        return Double.compare(inches.value, value) == 0;
     }
 }
